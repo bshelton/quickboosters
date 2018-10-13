9a76726583384e8d80775b4f7b2d22ebb48edf4a
bshelton <brockshelt@gmail.com>
2018-10-12 21:37:46 -0400

    added booster dashboard

--- booster/views.py
+++ booster/views.py
@@ -1,4 +1,10 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, request, render_template
 from flask_login import current_user
 
-boosterbp = Blueprint('boosterbp', __name__, template_folder='templates/booster/templates/', static_folder='static')
+boosterbp = Blueprint('boosterbp', __name__, template_folder='templates', static_folder='static/boosters')
+
+
+@boosterbp.route('/booster/dashboard')
+def booster_dashboard():
+
+    return render_template('/boosters/index.html')
