b36be6e328ff87a2cd312e4d19d1e12481d91563
Brock Shelton <goabshelto@gmail.com>
2020-05-20 15:23:31 -0400

    fix styling

--- quickboosters/api/order/dev/sample_data.py
+++ quickboosters/api/order/dev/sample_data.py
@@ -1,13 +1,20 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from quickboosters.api.order.models import Orders
+from quickboosters.api.order.models import Order_Status
 
 import datetime
 from quickboosters import db
 
 
 def create_order():
-  order = Orders(order_type="solo_boost",user_id='1',order_amount=69.42,status='completed',date_ordered=datetime.datetime.now(),game='league of legends')
-  print(order.user_id)
-  db.session.add(order)
-  db.session.commit()
+    order = Orders(order_type="solo_boost", 
+        user_id='1',
+        order_amount=69.42,
+        status=Order_Status.complete,
+        date_ordered=datetime.datetime.now(),
+        game='league of legends'
+    )
+    print(order.user_id)
+    db.session.add(order)
+    db.session.commit()
