b319a65d3c73a9cd118d7975b0dc56376d779fea
Jonah <jonahsturgill@gmail.com>
2020-05-21 12:44:17 -0400

    fixed order-service

--- quickboosters/api/order/service.py
+++ quickboosters/api/order/service.py
@@ -18,34 +18,35 @@ class OrderService:
         return Orders.query.all()
 
     @staticmethod
-    def get_by_id(Orders.id: int) -> Orders:
-    """Retrieve one oder matching a given id.
+    def get_by_id(order_id: int) -> Orders:
+        """Retrieve one order matching a given id.
 
-    Returns:
-        Order: one order with id given
-    """
-        return Orders.query.get(user_id)
+        Returns:
+            Order: one order with id given
+        """
+
+        return Orders.query.get(order_id)
 
     @staticmethod
     def create(attributes: OrdersInterface) -> Orders:
-    """ Creates a new order.
+        """ Creates a new order.
 
-    Parameters
-    ----------
-    attributes: OrderInterface
-        The attributes of an order
-    Returns: 
-        Order: The newly created order.
-    """
-    orders: Orders = orders(id=attributes['id'],
-    order_type=attributes["order_type"],
-    user_id=attributes["user_id"],
-    order_amount=attributes["order_amount"],
-    status=attributes["status"],
-    game=attributes["game"],
-    date_ordered=attributes["date_ordered"])
-    db.session.add(orders)
-    db.session.commit()
-        return Orders
+        Parameters
+        ----------
+        attributes: OrderInterface
+            The attributes of an order
+        Returns: 
+            Order: The newly created order.
+        """
+        orders: Orders = Orders(
+            order_type=attributes["order_type"],
+            user_id=attributes["user_id"],
+            order_amount=attributes["order_amount"],
+            status=attributes["status"],
+            game=attributes["game"],
+            date_ordered=attributes["date_ordered"])
+        db.session.add(orders)
+        db.session.commit()
+        return orders
 
     
